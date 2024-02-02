f198849b377726c821da5858227a87d810d49622
boots britton <boots.britton@yahoo.co.uk>
2024-02-02 15:22:56 +0000

    functionality done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "restaurants#index"
+  resources :restaurants,  only: [:new, :create, :show, :index] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+restaurants = [
+  { name: 'Chinese Restaurant', address: '11 china street', category: 'chinese' },
+  { name: 'Italian Restaurant', address: '22 italy street', category: 'italian' },
+  { name: 'Japanese Restaurant', address: '33 japan street', category: 'japanese' },
+  { name: 'French Restaurant', address: '44 france street', category: 'french' },
+  { name: 'Belgian Restaurant', address: '55 belgium street', category: 'belgian' }
+]
+
+# Seed restaurants
+restaurants.each do |restaurant_params|
+  Restaurant.create!(restaurant_params)
+  puts "done"
+end
